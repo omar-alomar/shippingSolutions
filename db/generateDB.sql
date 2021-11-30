34ac666d6e774f802b1579e0d905a601a41a4c75
Omar Alomar <omaro2000@gmail.com>
2021-11-30 16:55:19 -0500

    fixed generateDB.sql

--- db/generateDB.sql
+++ db/generateDB.sql
@@ -7,21 +7,12 @@ CREATE TABLE users (
     type VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    
-    PRIMARY KEY (id),
-    CONSTRAINT chkType CHECK (type IN ('driver', 'client', 'exec'))
-);
-
-CREATE TABLE clients (
-	id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    userId INT NOT NULL,
     address VARCHAR(255),
     pendingOrders INT,
     deliveredOrders INT,
     
     PRIMARY KEY (id),
-    FOREIGN KEY (userId) REFERENCES users(id)
+    CONSTRAINT chkType CHECK (type IN ('driver', 'client', 'exec'))
 );
 
 CREATE TABLE orders (
@@ -33,6 +24,61 @@ CREATE TABLE orders (
     driverName VARCHAR(255),
     
     PRIMARY KEY (id),
-    FOREIGN KEY (clientId) REFERENCES clients(id),
+    FOREIGN KEY (clientId) REFERENCES users(id),
     CONSTRAINT chkStatus CHECK (status IN ('Delivered', 'Pending'))
+);
+
+-- Adding some example users
+INSERT INTO users (
+    name,
+    type,
+    username,
+    password
+)
+VALUES 
+(
+    'Omar Alomar',
+    'exec',
+    'omaro2000',
+    'pass123'
+),
+(
+    'Mike Lawson',
+    'driver',
+    'mikey0',
+    'qwerty'
+),
+(
+    'Mahmoud Zaatari',
+    'driver',
+    'mohd44',
+    'qwerty123'
+);
+INSERT INTO users (
+    name,
+    type,
+    username,
+    password,
+    address,
+    pendingOrders,
+    deliveredOrders
+)
+VALUES 
+(
+    'Mark Scott',
+    'client',
+    'mark123',
+    'asf444',
+    '1432 Arroyo Lane',
+    0,
+    1
+),
+(
+    'Bob Lewis',
+    'client',
+    'mark123',
+    'tgif33',
+    '123 Campus Drive',
+    1,
+    1
 );
--- exec/views/orderEntry.php
+++ exec/views/orderEntry.php
@@ -41,7 +41,7 @@
                     <button type="submit" class="btn btn-dark" name="save">Submit</button>
                     </div>
                     </div>
-
+                    <!-- TODO: Add cancel button -->
 
                 </div>
             </form>
