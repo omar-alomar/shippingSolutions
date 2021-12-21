243cb08415cb7f5452abc4ff544ba5a0e9a60cf8
JaredHerrera6 <61523053+JaredHerrera6@users.noreply.github.com>
2021-12-21 00:06:41 -0500

    Order request able to send over to the phpadmin database

--- Clie/OrderRequest.php
+++ Clie/OrderRequest.php
@@ -1,70 +1,49 @@
+<?php
+include("config.php");
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
-    <link rel="styleSheet" href="sidebar.css">
-    <script src="sidebar.js"></script>
-    <title>SHipping Service Solutions</title>
+    </title>
 </head>
 
 <body>
-    <!--navbar-->
-    <div id="nav-placeholder"></div>
-    <script>
-        $(function() {
-            $("#nav-placeholder").load("common/nav.html");
-        });
-    </script>
-    <!--end of navbar-->
+    <form action="" method="POST">
+        Comppany Name<input type="text" name="name"><br>
+        Address <input type="text" name="address"><br>
+        Delivery Date<input type="date" name="dDate"><br>
+        Load Type <select name="LoadType">
+            <option>In</option>
+            <option>Out</option>
+        </select><br>
+        Number of Loads <input type="number" name="LoadsNum"><br>
+        <input type="submit" name="submit">
+    </form>
+    <?php
+    if (isset($_POST['submit'])) {
+        $name = $_POST['name'];
+        $address = $_POST['address'];
+        $dDate = $_POST['dDate'];
+        $LoadType = $_POST['LoadType'];
+        $LoadsNum = $_POST['LoadsNum'];
 
-    <div class="container">
-        <div class="row">
-            <nav class="navbar navbar-inverse fixed-top" id="sidebar-wrapper" role="navigation">
-                <ul class="nav sidebar-nav">
-                    <div class="sidebar-header">
-                        <div class="sidebar-brand">
-                            <a href="#"><img src="../img/logoTrans.png" width="150" height="60" alt=""></a>
-                        </div>
-                    </div>
-                    <li><a href="#deliveries">Deliveries</a></li>
-                    <li><a href="#reports">Generate report</a></li>
-                    <li><a href="#payroll">Payroll</a></li>
-                    <li><a href="#account">My account</a></li>
-                    <li><a href="#clients">Manage clients</a></li>
-                </ul>
-            </nav>
-            <div class="col-lg-8 col-lg-offset-2">
-                <h2>Request Order</h2>
-                <a href="#orderEntry" class="btn btn-dark" role="button">New order</a>
-                <table class="table table-bordered">
-                    <thead>
-                        <tr>
-                            <th scope="col">Address</th>
-                            <th scope="col">Type(In/Out)</th>
-                            <th scope="col">Number of Loads</th>
-                            <th scope="col">ID</th>
-                            <th scope="col">Confirm</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>york road</td>
-                            <td>Out</td>
-                            <td>5</td>
-                            <td>1231231</td>
-                            <td><button class="Confirm">Confirm Order</button></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
+        $result = "INSERT into request (name,address,dDate,LoadType,LoadsNum) values('$name','$address','$dDate','$LoadType','$LoadsNum')";
+        mysqli_query($mysqli, $result);
+        if ($result) {
+            echo "Success";
+        } else {
+            echo "Failed";
+        }
+    }
+    ?>
 </body>
 
 </html>
